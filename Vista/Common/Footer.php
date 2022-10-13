371828b3f79ed888a7991ad6731bd8ad3723a638
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-10-13 15:57:27 -0300

    arreglé los contenidos del footer y apliqué bootstrap, falta arreglar

--- Vista/Common/Footer.php
+++ Vista/Common/Footer.php
@@ -2,14 +2,15 @@
 <footer class="page-footer font-small">
 
   <!-- Copyright -->
-  <div class="container">
-      <div class="footer-copyright text-center py-3 text-light">Equipo Alfa Buena Onda :)
-      </div>
-      <div class="text-light">
-        CONTACTO <br>
-        Instagram: besto3d <br>
-        Whatsapp: +54 299 655 1374 
-      </div>
+  <div class="container row mt-2">
+    <div class="text-light col-9 ms-2 mb-2" id="infoContacto">
+      CONTACTO <br>
+      Instagram: besto3d <br>
+      Whatsapp: +54 299 655 1374 
+    </div>
+    <!-- Todavía no logro ubicar bien este div, tiene que quedar más pegado al lado derecho del footer  -->
+    <div class="footer-copyright text-center py-3 text-light col mx-0 mt-2" id="copyright">Equipo Alfa Buena Onda :)
+    </div>
   </div>
 </footer>
 </body>
