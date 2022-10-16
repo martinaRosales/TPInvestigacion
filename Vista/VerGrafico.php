95de10b212c9f3d26c4a4f6f339efb99433fef0b
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2022-10-16 17:00:42 -0300

    avances pdf

--- Vista/VerGrafico.php
+++ Vista/VerGrafico.php
@@ -2,20 +2,22 @@
 include('Common/Header.php');
 
 ?>
-<div class="container col-md-5 mt-3 ms-3">
+<div class="container col-md-12 mt-3 ms-3">
+<p><a href="generar_pdf_productos_disponibles" class="btn btn-primary">Descargar PDF de productos disponibles</a></p>
 <form action="" id="verGraficos">
     <div class="container">
         <h2 class="text-light">Ganancias Mensuales</h2>
         <button class="btn btn-primary" onClick="mostrarGrafico('VentasMensuales');">Ver</button>
+        <p></p>
         <?php include_once('GraficoVentasMensuales.php') ?>
     </div>
 
     <div class="container">
         <h2 class="text-light">Popularidad de productos</h2>
         <button class="btn btn-primary" onClick="mostrarGrafico('PopularidadProductos');">Ver</button>
+        <p></p>
         <?php include_once('GraficoPopularidadProductos.php') ?>
     </div>
 
-    <button class="btn btn-primary">Descargar PDF</button>
 </form>
 <script src="Assets/Js/VerGraficos.js"></script>
