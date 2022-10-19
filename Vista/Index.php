28e6790d09857df64d8dd8b23dff512ea11148d1
MartinaRosales <martina.rosales@est.fi.uncoma.edu.ar>
2022-10-19 20:23:52 -0300

    quite un id inutil

--- Vista/Index.php
+++ Vista/Index.php
@@ -2,7 +2,7 @@
 include_once("Common/Header.php");
 ?>
 
-<div class="container" style="margin:30px;height:80vh;" id="body">
+<div class="container" style="margin:30px;height:80vh;">
     <div class="container bg-dark" id="Index"> 
             <div class="display-1 text-light  text-center" id="titulo"><h1>¡Bienvenidxs al cat&aacute;logo Besto3D!</h1></div>
             <div class="container" id="contIndex" >
