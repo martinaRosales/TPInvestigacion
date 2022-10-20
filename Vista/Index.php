3daa8d4c3afba79da8f477259843ef540e7718ba
MartinaRosales <martina.rosales@est.fi.uncoma.edu.ar>
2022-10-20 09:33:14 -0300

    agregue el meme :)

--- Vista/Index.php
+++ Vista/Index.php
@@ -11,11 +11,14 @@ include_once("Common/Header.php");
                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
                     </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                         <img src="Assets/Img/Index/tarjetaBesto.jpg" id="imagenCarousel" class="d-block w-100" alt="Logo de Besto3D">
+                        </div>
+                        <div class="carousel-item ">
+                        <img src="Assets/Img/Index/bestoMeme.jpg" id="imagenCarousel" class="d-block w-100" alt="Meme Equipo Alfa Buena Onda :)">
                         </div>
                         <div class="carousel-item">
                         <img src="Assets/Img/Index/fotoStand.jpg" id="imagenCarousel" class="d-block w-100" alt="Imagen del stand">
